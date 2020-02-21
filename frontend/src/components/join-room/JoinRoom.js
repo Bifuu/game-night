79478f3b8585befc77db87211211cdc5fdba5478
Dustin Stover <DustinStover@gmail.com>
2020-02-21 12:32:51 -0500

    Adjust Join room ui

--- frontend/src/components/join-room/JoinRoom.js
+++ frontend/src/components/join-room/JoinRoom.js
@@ -14,9 +14,23 @@ export const JoinRoom = props => {
 
   return (
     <Join>
-      name:
-      <input type="text" onChange={e => setName(e.target.value)} value={name} />
-      <input type="text" onChange={e => setRoom(e.target.value)} value={room} />
+      <div>
+        name:
+        <input
+          type="text"
+          onChange={e => setName(e.target.value)}
+          value={name}
+        />
+      </div>
+      <div>
+        room:
+        <input
+          type="text"
+          onChange={e => setRoom(e.target.value)}
+          value={room}
+        />
+      </div>
+
       <button onClick={e => setUser(name, room)}>Join</button>
     </Join>
   );
